8a8f45c5cad875483b637ab2d254b8d66c8e0b60
ValentinSomoza <vsomoza@fi.uba.ar>
2024-11-11 21:57:43 -0300

    #3: fix: desing of pattern verification improved Co-authored-by: David<hcondori@fi.uba.ar> Co-authored-by: Matias  <mbagatto@fi.uba.ar> Co-authored-by: Juan Balella <jbalella@fi.uba.ar> Co-authored-by: Guiseppe Prieto Bonilla <gprietob@fi.uba.ar>

--- src/main/java/PlayerDeck.java
+++ src/main/java/PlayerDeck.java
@@ -2,20 +2,14 @@ import java.util.ArrayList;
 
 public class PlayerDeck {
     private ArrayList<Card> cards;
-    private ArrayList<Card> selectedCards;
     private static HandCalculator handCalculator = new HandCalculator();
 
     public PlayerDeck() {
         this.cards = new ArrayList<>();
-        this.selectedCards = new ArrayList<>();
     }
 
     public void addCard(Card card) {
         this.cards.add(card);
-    }
-
-    public void addSelectedCard(Card card) {
-        this.selectedCards.add(card);
     }
 
     public boolean isComplete() {
