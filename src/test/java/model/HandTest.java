50c3f78a138c348cee8e47bcbb754d39dfd52013
Matias <mbagatto@fi.uba.ar>
2024-11-18 22:12:27 -0300

    #14: fix: hand test

--- src/test/java/model/HandTest.java
+++ src/test/java/model/HandTest.java
@@ -1,14 +1,24 @@
 package model;
 
+import model.Cards.Card;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HandTest {
     @Test
-    public void verifyThatAHandCalculatesItsScoreCorrectly() {
-        Hand pair = new Hand(10, 2);
-        int expectedScore = 20;
-        assertEquals(expectedScore, pair.calculateScore());
+    public void verifyThatAPairCalculatesItsScoreCorrectly() {
+        // Arrange
+        ArrayList<Card> cards = new ArrayList<>();
+        cards.add(new Card(3, "heart"));
+        cards.add(new Card(3, "spade"));
+        Hand pair = new Hand(10, 2, cards);
+        Score expectedScore = new Score(32, 1);
+        // Act
+        Score obtainedScore = pair.calculateTotalScore();
+        // Assert
+        assertTrue(expectedScore.isEqualAs(obtainedScore));
     }
 }
