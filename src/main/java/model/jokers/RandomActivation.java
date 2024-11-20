7266fc399f64bfbff7274ed34c8a14c7e20aba28
ValentinSomoza <vsomoza@fi.uba.ar>
2024-11-20 20:20:43 -0300

    #23: Implementing RandomActivation Joker
    Co-authored-by: David <hcondori@fi.uba.ar>
    Co-authored-by: Matias <mbagatto@fi.uba.ar>
    Co-authored-by: Juan Balella <jbalella@fi.uba.ar>
    Co-authored-by: Giuseppe <gprietob@fi.uba.ar>

---
+++ src/main/java/model/jokers/RandomActivation.java
@@ -0,0 +1,15 @@
+package model.jokers;
+
+import model.hands.Hand;
+import model.score.Score;
+
+public class RandomActivation extends Joker {
+
+    public RandomActivation(String name, Score effect) {
+        super(name, effect );
+    }
+
+    public void applyEffect(Hand hand) {
+
+    }
+}
