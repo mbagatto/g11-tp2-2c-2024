8460d0456d852640010d512c7e4da30979a99bac
ValentinSomoza <vsomoza@fi.uba.ar>
2024-11-21 00:30:38 -0300

    #24: The Joker Combinated implemented succesfully
    Co-authored-by: David <hcondori@fi.uba.ar>
    Co-authored-by: Matias <mbagatto@fi.uba.ar>
    Co-authored-by: Juan Balella <jbalella@fi.uba.ar>
    Co-authored-by: Giuseppe <gprietob@fi.uba.ar>

--- src/test/java/model/JokerTest.java
+++ src/test/java/model/JokerTest.java
@@ -64,9 +64,10 @@ public class JokerTest {
         playerDeck.selectCard(0);
         Joker joker = new RandomActivationForMultiplier("Gros Michel", new Score(1, 15), new Probability(6));
         jokers.add(joker);
-        Score expectedScore = new Score(12, 15);
+        Score expectedScore = new Score(12, 16);
         // Act
         Score obtainedScore = playerDeck.playSelectedCards(jokers);
+
         while (!expectedScore.equals(obtainedScore)) {
             playerDeck.addCard(new Spade(7));
             playerDeck.selectCard(0);
@@ -83,7 +84,7 @@ public class JokerTest {
         PlayerDeck playerDeck = new PlayerDeck();
         playerDeck.addCard(new Spade(7));
         playerDeck.selectCard(0);
-        Joker joker = new RandomActivationForPoints("Manzano Dorado", new Score(500, 1), new Probability(20));
+        Joker joker = new RandomActivationForPoints("Golden Apple", new Score(500, 1), new Probability(20));
         jokers.add(joker);
         Score expectedScore = new Score(512, 1);
         // Act
@@ -96,4 +97,48 @@ public class JokerTest {
         // Assert
         assertEquals(expectedScore, obtainedScore);
     }
+
+    @Test
+    public void test05CombinatedJokerShouldCalculateCorrectlyAHandScore() {
+        // Arrange
+        ArrayList<Joker> jokersForCombinated = new ArrayList<>();
+        ArrayList<Joker> jokersForPlayerDeck = new ArrayList<>();
+        ArrayList<Card> cards = new ArrayList<>();
+        jokersForCombinated.add(new PlayedHandBonus("Second Joker", new Score(1, 3), new Straight(cards)));
+        jokersForCombinated.add(new ForTheScore("First Joker", new Score(1, 8)));
+        jokersForCombinated.add(new RandomActivationForMultiplier("Gros Michel", new Score(1, 15), new Probability(6)));
+        Joker joker = new Combinated("Combinated Supremacy", jokersForCombinated);
+        PlayerDeck playerDeck = new PlayerDeck();
+        playerDeck.addCard(new Spade(4));
+        playerDeck.addCard(new Diamond(6));
+        playerDeck.addCard(new Heart(5));
+        playerDeck.addCard(new Spade(3));
+        playerDeck.addCard(new Spade(7));
+        playerDeck.selectCard(0);
+        playerDeck.selectCard(1);
+        playerDeck.selectCard(2);
+        playerDeck.selectCard(3);
+        playerDeck.selectCard(4);
+        jokersForPlayerDeck.add(joker);
+        Score expectedScore = new Score(55, 30);
+        // Act
+        Score obtainedScore = playerDeck.playSelectedCards(jokersForPlayerDeck);
+
+        while (!expectedScore.equals(obtainedScore)) {
+            playerDeck.addCard(new Spade(4));
+            playerDeck.addCard(new Diamond(6));
+            playerDeck.addCard(new Heart(5));
+            playerDeck.addCard(new Spade(3));
+            playerDeck.addCard(new Spade(7));
+            playerDeck.selectCard(0);
+            playerDeck.selectCard(1);
+            playerDeck.selectCard(2);
+            playerDeck.selectCard(3);
+            playerDeck.selectCard(4);
+            obtainedScore = playerDeck.playSelectedCards(jokersForPlayerDeck);
+        }
+        // Assert
+        assertEquals(expectedScore, obtainedScore);
+    }
+
 }
