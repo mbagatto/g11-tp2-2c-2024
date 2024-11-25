9d36e42d8cfc95c72dc005b6746298aac2b9f618
Matias <mbagatto@fi.uba.ar>
2024-11-25 17:57:22 -0300

    test: added new unit tests for Player

--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -4,6 +4,11 @@ import model.cards.*;
 import model.decks.EnglishDeck;
 import model.exceptions.EmptyPlayerDeckException;
 import model.exceptions.NoSelectedCardsException;
+import model.jokers.DiscardBonus;
+import model.jokers.Joker;
+import model.score.Add;
+import model.score.DoNotModify;
+import model.score.Multiply;
 import model.score.Score;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -93,43 +98,117 @@ public class PlayerTest {
         assertDoesNotThrow(player::play);
     }
 
-//    @Test
-//    public void test05PlayerDiscardsWithDiscardBonusAndTheScoreIsCorrect() {
-//        // Arrange
-//        String playerName = "ExampleName";
-//        EnglishDeck englishDeck = new EnglishDeck();
-//        englishDeck.fillDeck();
-//        Player player = new Player(playerName, englishDeck);
-//        player.completeDeck();
-//        Joker joker = new ForPointsDB("Third Joker", new Score(10, 1));
-//        player.addJoker(joker);
-//        Score expectedScore = new Score(25, 1);
-//        // Act
-//        player.selectCard(0);
-//        player.discard();
-//        player.selectCard(1);
-//        Score obtainedScore = player.play();
-//        // Assert
-//        assertEquals(expectedScore, obtainedScore);
-//    }
+    @Test
+    public void test05PlayerDiscardsWithDiscardBonusAndTheMultiplierIsModifiedCorrectly() {
+        // Arrange
+        EnglishDeck englishDeck = new EnglishDeck();
+        englishDeck.fillDeck();
+        Player player = new Player("Lionel Messi", englishDeck);
+        player.completeDeck();
+        Joker joker = new DiscardBonus("Holy Discard", "", new DoNotModify(), new Multiply(new Score(20)));
+        player.addJoker(joker);
+        player.selectCard(0);
+        player.discard();
+        player.selectCard(0);
+        Score expectedScore = new Score(300);
+        // Act
+        Score obtainedScore = player.play();
+        // Assert
+        assertEquals(expectedScore, obtainedScore);
+    }
 
-//    @Test
-//    public void test06PlayerDiscardsWithDiscardBonusAndTheMultiplierIsAffectedCorrectly() {
-//        // Arrange
-//        String playerName = "ExampleName";
-//        EnglishDeck englishDeck = new EnglishDeck();
-//        englishDeck.fillDeck();
-//        Player player = new Player(playerName, englishDeck);
-//        player.completeDeck();
-//        Joker joker = new ForMultiplierDB("Fourth Joker", new Score(1, 15));
-//        player.addJoker(joker);
-//        Score expectedScore = new Score(15, 16);
-//        // Act
-//        player.selectCard(0);
-//        player.discard();
-//        player.selectCard(1);
-//        Score obtainedScore = player.play();
-//        // Assert
-//        assertEquals(expectedScore, obtainedScore);
-//    }
+    @Test
+    public void test06PlayerDoesNotDiscardWithDiscardBonusAndTheMultiplierIsNotModified() {
+        // Arrange
+        EnglishDeck englishDeck = new EnglishDeck();
+        englishDeck.fillDeck();
+        Player player = new Player("Lionel Messi", englishDeck);
+        player.completeDeck();
+        Joker joker = new DiscardBonus("Holy Discard", "", new DoNotModify(), new Multiply(new Score(20)));
+        player.addJoker(joker);
+        player.selectCard(0);
+        Score expectedScore = new Score(15);
+        // Act
+        Score obtainedScore = player.play();
+        // Assert
+        assertEquals(expectedScore, obtainedScore);
+    }
+
+    @Test
+    public void test07PlayerDiscardsTwiceWithDiscardBonusAndTheMultiplierIsModifiedCorrectly() {
+        // Arrange
+        EnglishDeck englishDeck = new EnglishDeck();
+        englishDeck.fillDeck();
+        Player player = new Player("Lionel Messi", englishDeck);
+        player.completeDeck();
+        Joker joker = new DiscardBonus("Holy Discard", "", new DoNotModify(), new Multiply(new Score(20)));
+        player.addJoker(joker);
+        player.selectCard(0);
+        player.discard();
+        player.selectCard(0);
+        player.discard();
+        player.selectCard(0);
+        Score expectedScore = new Score(6000);
+        // Act
+        Score obtainedScore = player.play();
+        // Assert
+        assertEquals(expectedScore, obtainedScore);
+    }
+
+    @Test
+    public void test08PlayerDiscardsWithDiscardBonusAndThePointsAreModifiedCorrectly() {
+        // Arrange
+        EnglishDeck englishDeck = new EnglishDeck();
+        englishDeck.fillDeck();
+        Player player = new Player("Lionel Messi", englishDeck);
+        player.completeDeck();
+        Joker joker = new DiscardBonus("Valley Ritual", "", new Add(new Score(75)), new DoNotModify());
+        player.addJoker(joker);
+        player.selectCard(0);
+        player.discard();
+        player.selectCard(0);
+        Score expectedScore = new Score(90);
+        // Act
+        Score obtainedScore = player.play();
+        // Assert
+        assertEquals(expectedScore, obtainedScore);
+    }
+
+    @Test
+    public void test09PlayerDiscardsTwiceWithDiscardBonusAndThePointsAreModifiedCorrectly() {
+        // Arrange
+        EnglishDeck englishDeck = new EnglishDeck();
+        englishDeck.fillDeck();
+        Player player = new Player("Lionel Messi", englishDeck);
+        player.completeDeck();
+        Joker joker = new DiscardBonus("Valley Ritual", "", new Add(new Score(75)), new DoNotModify());
+        player.addJoker(joker);
+        player.selectCard(0);
+        player.discard();
+        player.selectCard(0);
+        player.discard();
+        player.selectCard(0);
+        Score expectedScore = new Score(165);
+        // Act
+        Score obtainedScore = player.play();
+        // Assert
+        assertEquals(expectedScore, obtainedScore);
+    }
+
+    @Test
+    public void test10PlayerDiscardsTwiceWithDiscardBonusAndThePointsAreNotModified() {
+        // Arrange
+        EnglishDeck englishDeck = new EnglishDeck();
+        englishDeck.fillDeck();
+        Player player = new Player("Lionel Messi", englishDeck);
+        player.completeDeck();
+        Joker joker = new DiscardBonus("Valley Ritual", "", new Add(new Score(75)), new DoNotModify());
+        player.addJoker(joker);
+        player.selectCard(0);
+        Score expectedScore = new Score(15);
+        // Act
+        Score obtainedScore = player.play();
+        // Assert
+        assertEquals(expectedScore, obtainedScore);
+    }
 }
