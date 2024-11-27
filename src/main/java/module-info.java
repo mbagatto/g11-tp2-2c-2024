267aea3881cd8c50c44947bb56ee463394703890
ValentinSomoza <vsomoza@fi.uba.ar>
2024-11-27 13:50:46 -0300

    fix: tests fixed

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module balatro {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.fasterxml.jackson.databind;
-
-    exports view;
-    exports controller;
-    exports model;
-}
